ee004cadd6f5e0d74fcc208536cea90f4adfae95
JCat-World <siyavash.jcat@gmail.com>
2026-09-03 23:43:31 +0330

    local_settings removed

--- octashop_nili/envs/common.py
+++ octashop_nili/envs/common.py
@@ -3,7 +3,6 @@ import os
 from pathlib import Path
 from datetime import timedelta
 from decouple import config  # type: ignore
-from .local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
